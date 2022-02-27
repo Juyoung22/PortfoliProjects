43d095b35bff32c47e135719406fe9f973250edf
Juyoung22 <92056187+Juyoung22@users.noreply.github.com>
2022-02-27 12:02:28 -0500

    Add files via upload
    Data cleaning an excel file from data that I scrapped off from a website.
    the data is about a Nashville Property Information table.

---
+++ DataCleaning.sql
@@ -0,0 +1,150 @@
+
+-- Cleaning data with SQL queries
+
+SELECT *
+FROM dbo.NashvilleHousing
+
+-- Standardize Date Format
+
+SELECT SaleDate, CONVERT(Date, SaleDate) AS CleanedDate
+FROM dbo.NashvilleHousing
+
+ALTER TABLE NashvilleHousing
+ADD SaleDate2 Date;
+
+UPDATE NashvilleHousing
+SET SaleDate2 = CONVERT(Date, SaleDate);
+
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+-- Populate Property Address data
+
+SELECT *
+FROM dbo.NashvilleHousing
+ORDER BY ParcelID
+
+SELECT a.ParcelID, a.PropertyAddress, b.ParcelID, b.PropertyAddress, ISNULL(a.PropertyAddress, b.PropertyAddress)
+FROM dbo.NashvilleHousing a
+JOIN NashvilleHousing b
+	ON a.ParcelID = b.ParcelID
+	AND a.[UniqueID ] <> b.[UniqueID ]
+WHERE a.PropertyAddress is null
+
+UPDATE a
+SET PropertyAddress = ISNULL(a.PropertyAddress, b.PropertyAddress)
+FROM dbo.NashvilleHousing a
+JOIN NashvilleHousing b
+	ON a.ParcelID = b.ParcelID
+	AND a.[UniqueID ] <> b.[UniqueID ]
+WHERE a.PropertyAddress is null
+
+
+-- Breaking out Address into Individual Columns (Address, City, State)
+-- Breaking out Property Address
+
+SELECT PropertyAddress
+FROM NashvilleHousing
+
+SELECT
+SUBSTRING(PropertyAddress, 1 , CHARINDEX(',', PropertyAddress)-1) AS Address
+, SUBSTRING(PropertyAddress, CHARINDEX(',', PropertyAddress) +1, Len(PropertyAddress)) AS Address
+FROM NashvilleHousing
+
+ALTER TABLE NashvilleHousing
+ADD PropertySplitAddress NVARCHAR(255);
+
+UPDATE NashvilleHousing
+SET PropertySplitAddress = SUBSTRING(PropertyAddress, 1 , CHARINDEX(',', PropertyAddress)-1)
+
+ALTER TABLE NashvilleHousing
+ADD PropertySplitCity NVARCHAR(255);
+
+UPDATE NashvilleHousing
+SET PropertySplitCity = SUBSTRING(PropertyAddress, CHARINDEX(',', PropertyAddress) +1, Len(PropertyAddress))
+
+
+--Cleaning/Breaking Out Owner Address
+
+SELECT OwnerAddress
+FROM NashvilleHousing
+
+SELECT
+PARSENAME(REPLACE(OwnerAddress,',','.'), 3)
+,PARSENAME(REPLACE(OwnerAddress,',','.'), 2)
+,PARSENAME(REPLACE(OwnerAddress,',','.'), 1)
+FROM NashvilleHousing
+
+ALTER TABLE NashvilleHousing
+ADD OwnerSplitAddress NVARCHAR(255);
+
+UPDATE NashvilleHousing
+SET OwnerSplitAddress = PARSENAME(REPLACE(OwnerAddress,',','.'), 3)
+
+ALTER TABLE NashvilleHousing
+ADD OwnerSplitCity NVARCHAR(255);
+
+UPDATE NashvilleHousing
+SET OwnerSplitCity = PARSENAME(REPLACE(OwnerAddress,',','.'), 2)
+
+ALTER TABLE NashvilleHousing
+ADD OwnerSplitState NVARCHAR(255);
+
+UPDATE NashvilleHousing
+SET OwnerSplitState = PARSENAME(REPLACE(OwnerAddress,',','.'), 1)
+
+
+-- Change Y and N to Yes and No in 'Sold as Vacant' Column
+
+SELECT Distinct(SoldAsVacant), COUNT(SoldAsVacant)
+FROM NashvilleHousing
+GROUP BY SoldAsVacant
+ORDER BY 2
+
+SELECT SoldAsVacant,
+CASE WHEN SoldAsVacant = 'y' THEN 'Yes'
+	WHEN SoldAsVacant = 'n' THEN 'No'
+	ELSE SoldAsVacant
+	END
+FROM NashvilleHousing
+
+UPDATE NashvilleHousing
+SET SoldAsVacant = CASE WHEN SoldAsVacant = 'Y' THEN 'Yes'
+	WHEN SoldAsVacant = 'N' THEN 'No'
+	ELSE SoldAsVacant
+	END
+
+
+
+
+
+-- Removing Duplicates
+WITH RowNumCTE AS(
+SELECT *, ROW_NUMBER() OVER (Partition By ParcelID, PropertyAddress, SalePrice, SaleDate, LegalReference ORDER BY UniqueID) row_num
+FROM NashvilleHousing
+)
+DELETE
+FROM RowNumCTE
+WHERE row_num > 1
+
+WITH RowNumCTE AS(
+SELECT *, ROW_NUMBER() OVER (Partition By ParcelID, PropertyAddress, SalePrice, SaleDate, LegalReference ORDER BY UniqueID) row_num
+FROM NashvilleHousing
+)
+SELECT *
+FROM RowNumCTE
+WHERE row_num > 1
+-- I use CTE because I can't use conditions like 'WHERE' function from alias row_num unless I make a temp table and call it from a subquery.
+
+
+
+-- Deleting Unused Columns
+
+
+SELECT *
+FROM NashvilleHousing
+
+ALTER TABLE NashvilleHousing
+DROP COLUMN OwnerAddress, TaxDistrict, PropertyAddress
+
+ALTER TABLE NashvilleHousing
+DROP COLUMN SaleDate
+
